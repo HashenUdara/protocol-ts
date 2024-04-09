c1288fe3c6130f7b14de718490e273bda5a5fd81
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-04-09 09:53:20 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import glob from 'fast-glob'
-
+import { Inter as FontSans } from 'next/font/google'
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
 import { type Metadata } from 'next'
 import { type Section } from '@/components/SectionProvider'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
   title: {
@@ -30,7 +35,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
-      <body className="flex min-h-full bg-white antialiased dark:bg-neutral-950">
+      <body
+        className={`flex min-h-full bg-white antialiased dark:bg-neutral-950`}
+      >
         <Providers>
           <div className="w-full">
             <Layout allSections={allSections}>{children}</Layout>
